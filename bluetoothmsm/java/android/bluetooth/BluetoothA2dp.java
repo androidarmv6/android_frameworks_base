d5e1f7e065a2b4fc4795ae1a4e3fcb72e893c6c4
Sumit Bajpai <sbajpai@codeaurora.org>
2013-01-25 15:50:32 +0530

    Bluetooth: Implementing finalize() method.
    Added finalize() in BluetoothA2dp.java for
    clean up in this method.
    
    Change-Id: I4f23333db782f8d9fc6fadb41d0b33fbe694329a
    CRs-Fixed: 443585

--- bluetoothmsm/java/android/bluetooth/BluetoothA2dp.java
+++ bluetoothmsm/java/android/bluetooth/BluetoothA2dp.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (c) 2013 The Linux Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -131,6 +132,10 @@ public final class BluetoothA2dp implements BluetoothProfile {
 
     /*package*/ void close() {
         mServiceListener = null;
+    }
+
+    public void finalize() {
+        close();
     }
 
     /**
