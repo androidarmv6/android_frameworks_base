ed7b8daff5dc0a06abd26c5b0c2bb0a770048666
AndroidMeda <hephappy@gmail.com>
2013-07-25 14:34:31 +0200

    Revert "Support devices with SoftAp that don't require firmware reload"
    This reverts commit a582ded3e17818e75e4bbfbfe39ef29cb0959b82.
    
    Conflicts:
    	core/res/res/values/symbols.xml
    
    Change-Id: Ia52abeb661cd4fc7e5101a90fb7bd8ccedd061ab

--- services/java/com/android/server/NetworkManagementService.java
+++ services/java/com/android/server/NetworkManagementService.java
@@ -37,7 +37,6 @@ import static com.android.server.NetworkManagementSocketTagger.PROP_QTAGUID_ENAB
 
 import android.bluetooth.BluetoothTetheringDataTracker;
 import android.content.Context;
-import android.content.res.Resources;
 import android.net.INetworkManagementEventObserver;
 import android.net.InterfaceConfiguration;
 import android.net.LinkAddress;
@@ -1018,13 +1017,9 @@ public class NetworkManagementService extends INetworkManagementService.Stub
             WifiConfiguration wifiConfig, String wlanIface) {
         mContext.enforceCallingOrSelfPermission(CONNECTIVITY_INTERNAL, TAG);
         try {
-            Resources resources = mContext.getResources();
-            if (resources.getBoolean(
-                        com.android.internal.R.bool.config_wifiApFirmwareReload)) {
-                wifiFirmwareReload(wlanIface, "AP");
-            }
+            wifiFirmwareReload(wlanIface, "AP");
 
-            if (resources.getBoolean(
+            if (mContext.getResources().getBoolean(
                         com.android.internal.R.bool.config_wifiApStartInterface)) {
                 mConnector.execute("softap", "start", wlanIface);
             }
