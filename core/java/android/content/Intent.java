2b81d1ae15ba66360aaa1dcd931d7ee5283e33f9
Francesco Colella <mnemonyc@hotmail.com>
2013-09-02 15:53:52 +0200

    Halo 2.0 (1/2)
    Ported HALO from ParanoidAndroid

--- core/java/android/content/Intent.java
+++ core/java/android/content/Intent.java
@@ -3533,7 +3533,12 @@ public class Intent implements Parcelable, Cloneable {
      */
     public static final int FLAG_ACTIVITY_TASK_ON_HOME = 0X00004000;
     /**
-     * If set, when sending a broadcast only registered receivers will be
+     * If set, this intent will always match start up as a floating window
+     * in mutil window scenarios.
+     */
+    public static final int FLAG_FLOATING_WINDOW = 0x00002000;   
+    /**
+      * If set, when sending a broadcast only registered receivers will be
      * called -- no BroadcastReceiver components will be launched.
      */
     public static final int FLAG_RECEIVER_REGISTERED_ONLY = 0x40000000;
