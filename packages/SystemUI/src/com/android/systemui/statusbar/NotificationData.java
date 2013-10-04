cbc05b33bd9a916a3166293823ac141708269ee0
Francesco Colella <mnemonyc@hotmail.com>
2013-10-04 11:22:46 +0200

    Halo 2.0 (step 1/2)
    not merge...only for test.

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationData.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationData.java
@@ -17,10 +17,12 @@
 package com.android.systemui.statusbar;
 
 import android.service.notification.StatusBarNotification;
+import android.graphics.Bitmap; 
 import android.os.IBinder;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.android.systemui.statusbar.BaseStatusBar.NotificationClicker; 
 import com.android.systemui.R;
 
 import java.util.Comparator;
@@ -37,8 +39,12 @@ public class NotificationData {
         public View row; // the outer expanded view
         public View content; // takes the click events and sends the PendingIntent
         public View expanded; // the inflated RemoteViews
+        public View haloContent; 
         public ImageView largeIcon;
+        protected boolean hide = false;
+        protected Bitmap roundIcon;
         protected View expandedLarge;
+        protected NotificationClicker floatingIntent; 
         public Entry() {}
         public Entry(IBinder key, StatusBarNotification n, StatusBarIconView ic) {
             this.key = key;
@@ -52,6 +58,12 @@ public class NotificationData {
         public View getLargeView() {
             return expandedLarge;
         }
+        public NotificationClicker getFloatingIntent() {
+            return floatingIntent;
+        }
+        public Bitmap getRoundIcon() {
+            return roundIcon;
+        } 
         /**
          * Return whether the entry can be expanded.
          */
