69c75af11fea0381aef77fdc6dd91d48efc112c8
Alin <alin.p32@gmail.com>
2014-02-04 21:26:18 +0000

    Re-arrange OOM killer to account for SERVICE_ADJ and PREVIOUS_APP_ADJ in slots
    See: http://forum.xda-developers.com/showpost.php?p=50044022&postcount=1500
    
    Change-Id: I360359c6f073aab7fd7b2e00afa72618120fe006

--- services/java/com/android/server/am/ProcessList.java
+++ services/java/com/android/server/am/ProcessList.java
@@ -145,14 +145,14 @@ final class ProcessList {
     // the OOM killer.  Note that the OOM killer only supports 6 slots, so we
     // can't give it a different value for every possible kind of process.
     private final int[] mOomAdj = new int[] {
-            FOREGROUND_APP_ADJ, VISIBLE_APP_ADJ, PERCEPTIBLE_APP_ADJ,
-            BACKUP_APP_ADJ, CACHED_APP_MIN_ADJ, CACHED_APP_MAX_ADJ
+            FOREGROUND_APP_ADJ, PERCEPTIBLE_APP_ADJ, SERVICE_ADJ,
+            PREVIOUS_APP_ADJ, CACHED_APP_MIN_ADJ, CACHED_APP_MAX_ADJ
     };
     // These are the low-end OOM level limits.  This is appropriate for an
     // HVGA or smaller phone with less than 512MB.  Values are in KB.
     private final long[] mOomMinFreeLow = new long[] {
-            8192, 12288, 16384,
-            24576, 28672, 32768
+            4096, 8192, 16384,
+            24576, 32768, 40960
     };
     // These are the high-end OOM level limits.  This is appropriate for a
     // 1280x800 or larger screen with around 1GB RAM.  Values are in KB.
