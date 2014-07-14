590e034461cfb93d2e2eb3ec6b2fe902bd48c1f0
Ravit Dennis <rdennis@codeaurora.org>
2014-07-14 17:45:23 +0300

    Ultrasound: Rotate DigitalPenOffScreenDisplay when main display is rotated
    Main display may sometimes mirror coordinates received on
    the DigitalPenOffScreenDisplay. When the main display is rotated
    and the external display is not, the coordinate systems do not match and
    the coordinates received seem off.
    This change would rotate the external display whenever the main display
    is rotated to address that issue.
    
    Change-Id: I08a8359c70cd7966d6b0a981b7a2f24400ef671e

--- services/java/com/android/server/display/DigitalPenOffScreenDisplayAdapter.java
+++ services/java/com/android/server/display/DigitalPenOffScreenDisplayAdapter.java
@@ -52,12 +52,12 @@ import java.lang.reflect.Method;
  * Display adapters are guarded by the {@link DisplayManagerService.SyncRoot} lock.
  * </p>
  */
-final class DigitalPenOffScreenDisplayAdapter extends DisplayAdapter {
+public final class DigitalPenOffScreenDisplayAdapter extends DisplayAdapter {
     private static final String TAG = "DigitalPenOffScreenDisplayAdapter";
     private DigitalPenOffScreenDisplayDevice mDevice;
     public static final String DISPLAY_NAME = "Digital Pen off-screen display";
     private static final boolean mDigitalPenCapable =
-      Resources.getSystem().getBoolean(com.android.internal.R.bool.config_digitalPenCapable);
+    Resources.getSystem().getBoolean(com.android.internal.R.bool.config_digitalPenCapable);
     private DimensionsCallback mCallback;
 
     // Called with SyncRoot lock held.
@@ -72,7 +72,7 @@ final class DigitalPenOffScreenDisplayAdapter extends DisplayAdapter {
     }
 
     public static String getDisplayName() {
-      return DISPLAY_NAME;
+        return DISPLAY_NAME;
     }
 
     @Override
@@ -95,20 +95,20 @@ final class DigitalPenOffScreenDisplayAdapter extends DisplayAdapter {
         Class digitalPenServiceClass = digitalPenService.getClass();
         try {
             Class args[] = {
-              IDigitalPenDimensionsCallback.class
+                IDigitalPenDimensionsCallback.class
             };
 
             Method registerCallbackMethod =
-              digitalPenServiceClass.getMethod("registerOffScreenDimensionsCallback", args);
+            digitalPenServiceClass.getMethod("registerOffScreenDimensionsCallback", args);
             registerCallbackMethod.invoke(digitalPenService,
                                           mCallback);
         } catch (InvocationTargetException e) {
             Slog.i(TAG,
-              "DigitalPenService.registerOffScreenDimensionsCallback - InvocationTargetException");
+                   "DigitalPenService.registerOffScreenDimensionsCallback - InvocationTargetException");
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             Slog.i(TAG,
-              "DigitalPenService.registerOffScreenDimensionsCallback - IllegalAccessException");
+                   "DigitalPenService.registerOffScreenDimensionsCallback - IllegalAccessException");
             e.printStackTrace();
         } catch (Throwable t) {
             Slog.i(TAG, "DigitalPenService.registerOffScreenDimensionsCallback Not available.");
@@ -117,66 +117,66 @@ final class DigitalPenOffScreenDisplayAdapter extends DisplayAdapter {
     }
 
     private final class DigitalPenOffScreenDisplayDevice extends DisplayDevice {
-      private String mName;
-      private int mWidth;
-      private int mHeight;
-      private final float mRefreshRate;
-      private final int mDensityDpi;
-      private final float mXDpi;
-      private final float mYDpi;
-      private final int mFlags;
-      private final int mType;
-      private final int mTouch;
-      private DisplayDeviceInfo mInfo;
+        private String mName;
+        private int mWidth;
+        private int mHeight;
+        private final float mRefreshRate;
+        private final int mDensityDpi;
+        private final float mXDpi;
+        private final float mYDpi;
+        private final int mFlags;
+        private final int mType;
+        private final int mTouch;
+        private DisplayDeviceInfo mInfo;
 
-      public DigitalPenOffScreenDisplayDevice(IBinder displayToken) {
-          super(DigitalPenOffScreenDisplayAdapter.this, displayToken);
-          mName = getDisplayName();
-          mWidth = 480;
-          mHeight = 640;
-          mRefreshRate = 60;
-          mDensityDpi = DisplayMetrics.DENSITY_DEFAULT;
-          mXDpi = 160;
-          mYDpi = 160;
-          mFlags = 0;
-          mType = Display.TYPE_BUILT_IN;
-          mTouch = DisplayDeviceInfo.TOUCH_EXTERNAL;
-      }
-
-      @Override
-      public DisplayDeviceInfo getDisplayDeviceInfoLocked() {
-        if (mInfo == null) {
-          Slog.i(TAG, "mInfo is null, getting a new one");
-          mInfo = new DisplayDeviceInfo();
-          mInfo.name = mName;
-          mInfo.width = mWidth;
-          mInfo.height = mHeight;
-          mInfo.refreshRate = mRefreshRate;
-          mInfo.densityDpi = mDensityDpi;
-          mInfo.xDpi = mXDpi;
-          mInfo.yDpi = mYDpi;
-          mInfo.flags = mFlags;
-          mInfo.type = mType;
-          mInfo.touch = mTouch;
+        public DigitalPenOffScreenDisplayDevice(IBinder displayToken) {
+            super(DigitalPenOffScreenDisplayAdapter.this, displayToken);
+            mName = getDisplayName();
+            mWidth = 480;
+            mHeight = 640;
+            mRefreshRate = 60;
+            mDensityDpi = DisplayMetrics.DENSITY_DEFAULT;
+            mXDpi = 160;
+            mYDpi = 160;
+            mFlags = DisplayDeviceInfo.FLAG_ROTATES_WITH_CONTENT;
+            mType = Display.TYPE_BUILT_IN;
+            mTouch = DisplayDeviceInfo.TOUCH_EXTERNAL;
         }
-        return mInfo;
-      }
 
-      public void setDimesionsLocked(int width, int height) {
-              mHeight = height;
-              mWidth = width;
-              mInfo = null;
-      }
+        @Override
+        public DisplayDeviceInfo getDisplayDeviceInfoLocked() {
+            if (mInfo == null) {
+                Slog.i(TAG, "mInfo is null, getting a new one");
+                mInfo = new DisplayDeviceInfo();
+                mInfo.name = mName;
+                mInfo.width = mWidth;
+                mInfo.height = mHeight;
+                mInfo.refreshRate = mRefreshRate;
+                mInfo.densityDpi = mDensityDpi;
+                mInfo.xDpi = mXDpi;
+                mInfo.yDpi = mYDpi;
+                mInfo.flags = mFlags;
+                mInfo.type = mType;
+                mInfo.touch = mTouch;
+            }
+            return mInfo;
+        }
+
+        public void setDimesionsLocked(int width, int height) {
+            mHeight = height;
+            mWidth = width;
+            mInfo = null;
+        }
     }
 
     private final class DimensionsCallback extends IDigitalPenDimensionsCallback.Stub {
-      public void onDimensionsChange(int width, int height) {
-        Slog.i(TAG, "DimensionsCallback was called with w: " + width + " h: " + height);
-        synchronized (getSyncRoot()) {
-          mDevice.setDimesionsLocked(width, height);
-          sendDisplayDeviceEventLocked(mDevice, DISPLAY_DEVICE_EVENT_REMOVED);
-          sendDisplayDeviceEventLocked(mDevice, DISPLAY_DEVICE_EVENT_ADDED);
+        public void onDimensionsChange(int width, int height) {
+            Slog.i(TAG, "DimensionsCallback was called with w: " + width + " h: " + height);
+            synchronized (getSyncRoot()) {
+                mDevice.setDimesionsLocked(width, height);
+                sendDisplayDeviceEventLocked(mDevice, DISPLAY_DEVICE_EVENT_REMOVED);
+                sendDisplayDeviceEventLocked(mDevice, DISPLAY_DEVICE_EVENT_ADDED);
+            }
         }
-      }
     }
 }
--- services/java/com/android/server/display/LogicalDisplay.java
+++ services/java/com/android/server/display/LogicalDisplay.java
@@ -258,8 +258,10 @@ final class LogicalDisplay {
         // The orientation specifies how the physical coordinate system of the display
         // is rotated when the contents of the logical display are rendered.
         int orientation = Surface.ROTATION_0;
-        if (device == mPrimaryDisplayDevice
-                && (displayDeviceInfo.flags & DisplayDeviceInfo.FLAG_ROTATES_WITH_CONTENT) != 0) {
+        if ((device == mPrimaryDisplayDevice ||
+             (device.getNameLocked().equals(DigitalPenOffScreenDisplayAdapter.getDisplayName()) &&
+              !DigitalPenOffScreenDisplayAdapter.isDigitalPenDisabled()))
+            && (displayDeviceInfo.flags & DisplayDeviceInfo.FLAG_ROTATES_WITH_CONTENT) != 0) {
             orientation = displayInfo.rotation;
         }
 
